a0c4a97c3028ffbc14662a062ddaadcea5085cc8
claytongandra <claytongandra@ymail.com>
2017-02-16 13:06:44 -0200

    Aula 5

---
+++ Clayton.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Clayton.LojaVirtual.Dominio.Entidade
+{
+    public class Produto
+    {
+        public int ProdutoId {  get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
