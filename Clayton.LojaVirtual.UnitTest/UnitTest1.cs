2c160a9bd545182bacc03c70bf6463221af71f0e
claytongandra <claytongandra@ymail.com>
2017-02-16 12:34:25 -0200

    First Commit

---
+++ Clayton.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Clayton.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestLojaVirtual
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
