670481536bb3fe4eb629d147a8fc595e1af9ef98
claytongandra <claytongandra@ymail.com>
2017-03-09 19:40:32 -0300

    Lesson 06 Complet

---
+++ Clayton.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Clayton.LojaVirtual.Dominio.Entidade;
+
+namespace Clayton.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext: DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+
+        }
+    }
+}
