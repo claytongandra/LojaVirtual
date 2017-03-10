eca217d55f0009d2b5f9f9c598774e595d4e5d52
claytongandra <claytongandra@ymail.com>
2017-03-09 21:08:32 -0300

    Lesson 08 complete

---
+++ Clayton.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using Clayton.LojaVirtual.Web.Models;
+
+namespace Clayton.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        //Extenção da classe HtmlHelper retornando um MvcHtmlString
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int,string> paginaUrl )
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if(i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
