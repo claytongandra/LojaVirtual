be8911ce0a704f2660d596f0d5e55a5da20ea66b
claytongandra <claytongandra@ymail.com>
2017-03-09 22:25:15 -0300

    Lesson 10 complete

--- Clayton.LojaVirtual.UnitTest/UnitTest1.cs
+++ Clayton.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Web;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clayton.LojaVirtual.Web.Models;
+using Clayton.LojaVirtual.Web.HtmlHelpers;
 
 namespace Clayton.LojaVirtual.UnitTest
 {
@@ -7,8 +12,30 @@ namespace Clayton.LojaVirtual.UnitTest
     public class UnitTestLojaVirtual
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestarGeracaoPaginacao()
         {
+            //Arrange
+            HtmlHelper htmlHelper = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+                
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //Act
+            MvcHtmlString resultado = htmlHelper.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+
+            Assert.AreEqual(
+                 @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                 + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                 + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                 );
         }
     }
 }
---
+++ Clayton.LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Clayton.LojaVirtual.Dominio.Entidade;
+
+namespace Clayton.LojaVirtual.Web.Models
+{
+    public class ProdutosViewModel
+    {
+        public IEnumerable<Produto> Produtos { get; set; }
+        public Paginacao Paginacao { get; set; }
+        
+    }
+}
